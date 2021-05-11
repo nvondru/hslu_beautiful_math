5f2221d62b3e51843a0955a33fb856b68de887ca
Nicolas Vondru <niki.vondru@hotmail.ch>
2021-05-12 00:24:38 +0200

    donut done!

--- sketches/SW_03/Shell/sketch.js
+++ sketches/SW_03/Shell/sketch.js
@@ -1,8 +1,8 @@
-let circlePoints = 20;
+let circlePoints = 10;
 let circleWidth = 50;
 let circleHeight = 50;
 
-let angleStep = 360 / 10;
+let angleStep = 360 / 60;
 let width = 100;
 let height = 100;
 
@@ -30,23 +30,54 @@ function draw() {
   drawGuides();
   createVertices();
   drawVertices();
+  randomSeed(93);
 
   circlesVertices = [];
 }
 
 // create circle vertices rotated around a circle
 function createVertices() {
-  // for (let i = 1; i < 360; i += angleStep) {
-  //   let x = width * sin(i);
-  //   let z = height * cos(i);
-  //   let center = { x: x, y: 0, z: z };
-
-  // }
-
-  // old
-  for (let i = 0; i < 20; i++) {
-    circlesVertices.push(createCircleVertices(i * -20));
+  for (let i = 0; i <= 360; i += angleStep) {
+    let x = width * sin(i);
+    let y = height * cos(i);
+    let z = 0;
+    let center = { x, y, z };
+    circlesVertices.push(createCircleVertices(center, i));
   }
+}
+
+function createCircleVertices(center, deg) {
+  let angleStep = 360 / circlePoints;
+  let vertices = [];
+  let origin = vec3.fromValues(center.x, center.y, center.z);
+
+  for (let i = 1; i < 360; i += angleStep) {
+    let x = center.x + circleWidth * sin(i);
+    let y = center.y + circleHeight * cos(i);
+    let z = center.z;
+
+    let vertexVector = vec3.fromValues(x, y, z);
+    let transformedVector = vec3.create();
+    vec3.rotateY(
+      transformedVector,
+      vertexVector,
+      origin,
+      glMatrix.toRadian(90)
+    );
+    vec3.rotateZ(
+      transformedVector,
+      transformedVector,
+      origin,
+      glMatrix.toRadian(-deg)
+    );
+
+    vertices.push({
+      x: transformedVector[0],
+      y: transformedVector[1],
+      z: transformedVector[2],
+    });
+  }
+  return vertices;
 }
 
 function drawVertices() {
@@ -56,6 +87,12 @@ function drawVertices() {
 
     // for each point of a circle
     for (let j = 0; j < currentCircle.length; j++) {
+      let r = random(60);
+      let g = random(256);
+      let b = random(120);
+      noStroke();
+
+      fill(r, g, b);
       // if not last circle
       if (i + 1 < circlesVertices.length) {
         let nextCircle = circlesVertices[i + 1];
@@ -79,8 +116,9 @@ function drawGuides() {
   beginShape();
   for (let i = 1; i < 360; i += angleStep) {
     let x = width * sin(i);
-    let z = height * cos(i);
-    vertex(x, 0, z);
+    let y = height * cos(i);
+    let z = 0;
+    vertex(x, y, z);
   }
   endShape(CLOSE);
   pop();
@@ -115,16 +153,4 @@ function drawPolygon(vertex_1, vertex_2, vertex_3) {
   vertex(vertex_2.x, vertex_2.y, vertex_2.z);
   vertex(vertex_3.x, vertex_3.y, vertex_3.z);
   endShape();
-}
-
-function createCircleVertices(z) {
-  let angleStep = 360 / circlePoints;
-  let vertices = [];
-
-  for (let i = 1; i < 360; i += angleStep) {
-    let x = circleWidth * sin(i);
-    let y = circleHeight * cos(i);
-    vertices.push({ x, y, z });
-  }
-  return vertices;
 }
