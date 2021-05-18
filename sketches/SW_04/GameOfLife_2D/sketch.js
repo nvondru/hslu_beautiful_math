af9df4979f070f85f8911b16db2b745f8c5a6c0e
Nicolas Vondru <nicolas.vondru@stud.hslu.ch>
2021-05-19 00:35:24 +0200

    add 2D variant

--- sketches/SW_04/GameOfLife_2D/sketch.js
+++ sketches/SW_04/GameOfLife_2D/sketch.js
@@ -1,59 +1,111 @@
-let fieldWidth;
-let fieldHeight;
-
-let ruleInput;
-let startInput;
-
 let currentGrid;
 let nextGrid;
 
+let canvasWidth = 800;
+let canvasHeight = 800;
+
+let gridDimension = 100;
+let fieldWidth = canvasWidth / gridDimension;
+let fieldHeight = canvasHeight / gridDimension;
+
+let fps = 5;
+
 function setup() {
-  createCanvas(800, 800);
-
-  noStroke();
-
-  ruleInput = document.getElementById("ruleInput");
-  startInput = document.getElementById("startInput");
-
+  createCanvas(canvasWidth, canvasHeight);
+  frameRate(fps);
+  // noStroke();
+  // initialize seed
   currentGrid = [];
-
-  for (let i = 0; i < 11; i++) {
+  for (let i = 0; i < gridDimension; i++) {
     let currentRow = [];
-    for (let j = 0; j < 11; j++) {
-      if (i == 5 && j == 5) {
-        currentRow[j] = 1;
-      } else {
-        currentRow[j] = 0;
+    for (let j = 0; j < gridDimension; j++) {
+      currentRow[j] = {
+        x: j,
+        y: i,
+        isAlive: false,
+      };
+      if (
+        i >= Math.floor(gridDimension / 2) - Math.floor(gridDimension / 5) &&
+        i <= Math.floor(gridDimension / 2) + Math.floor(gridDimension / 5) &&
+        j >= Math.floor(gridDimension / 2) - Math.floor(gridDimension / 5) &&
+        j <= Math.floor(gridDimension / 2) + Math.floor(gridDimension / 5)
+      ) {
+        currentRow[j].isAlive = true;
       }
     }
     currentGrid.push(currentRow);
   }
-
-  console.log(currentGrid);
+  drawCurrentGrid();
 }
 
 function draw() {
   background(220);
-
-  // create nextGrid
-  for (let i = 0; i < currentGrid.length; i++) {
-    const row = currentGrid[i];
-
-    for (let j = 0; j < row.length; j++) {
-      const field = row[j];
-      let neighbours = getNeighbours(i, j, currentGrid.length, row.length);
-      // check how man alive
-      // switch cases
-      // set state of i/j accoridngly
-      // push to next
-      //at the end set next as current
-      // draw current
-      // repeat
-    }
-  }
+  tick();
+  drawCurrentGrid();
 }
 
-function getNeighbours(i, j, rowCount, columnCount) {
+function drawCurrentGrid() {
+  currentGrid.forEach((row) => {
+    row.forEach((field) => {
+      if (field.isAlive) {
+        fill(0);
+        stroke(0);
+      } else {
+        fill(255);
+        stroke(180);
+      }
+      rect(
+        field.x * fieldWidth,
+        field.y * fieldHeight,
+        fieldWidth,
+        fieldHeight
+      );
+    });
+  });
+}
+
+function tick() {
+  nextGrid = [];
+  for (let y = 0; y < gridDimension; y++) {
+    nextGrid[y] = [];
+  }
+  currentGrid.forEach((row) => {
+    row.forEach((currentField) => {
+      let neighbours = getNeighbours(
+        currentField,
+        currentGrid.length,
+        row.length
+      );
+      let aliveNeighbours = neighbours.filter((neighbour) => {
+        return neighbour.isAlive;
+      }).length;
+
+      let currentField_copy = {
+        x: currentField.x,
+        y: currentField.y,
+        isAlive: currentField.isAlive,
+      };
+
+      if (currentField.isAlive && aliveNeighbours < 2) {
+        currentField_copy.isAlive = false;
+      } else if (
+        (currentField.isAlive && aliveNeighbours === 2) ||
+        (currentField.isAlive && aliveNeighbours === 3)
+      ) {
+        currentField_copy.isAlive = true;
+      } else if (currentField.isAlive && aliveNeighbours > 3) {
+        currentField_copy.isAlive = false;
+      } else if (!currentField.isAlive && aliveNeighbours === 3) {
+        currentField_copy.isAlive = true;
+      }
+
+      nextGrid[currentField.y][currentField.x] = currentField_copy;
+    });
+  });
+  currentGrid = nextGrid;
+}
+
+function getNeighbours(currentField, rowCount, columnCount) {
   let neighbours = [];
   let indexTop;
   let indexBottom;
@@ -61,41 +113,42 @@ function getNeighbours(i, j, rowCount, columnCount) {
   let indexRight;
 
   // get index of row above
-  if (i - 1 < 0) {
+  if (currentField.y - 1 < 0) {
     indexTop = rowCount - 1;
   } else {
-    indexTop = i - 1;
+    indexTop = currentField.y - 1;
   }
 
   // get index of row below
-  if (i + 1 > rowCount - 1) {
+  if (currentField.y + 1 > rowCount - 1) {
     indexBottom = 0;
   } else {
-    indexBottom = i + 1;
+    indexBottom = currentField.y + 1;
   }
 
   // get index of column left
-  if (j - 1 < 0) {
+  if (currentField.x - 1 < 0) {
     indexLeft = columnCount - 1;
   } else {
-    indexLeft = j - 1;
+    indexLeft = currentField.x - 1;
   }
 
   // get index of column right
-  if (j + 1 > columnCount - 1) {
+  if (currentField.x + 1 > columnCount - 1) {
     indexRight = 0;
   } else {
-    indexRight = j + 1;
+    indexRight = currentField.x + 1;
   }
 
   neighbours.push(currentGrid[indexTop][indexLeft]);
-  neighbours.push(currentGrid[indexTop][j]);
+  neighbours.push(currentGrid[indexTop][currentField.x]);
   neighbours.push(currentGrid[indexTop][indexRight]);
-  neighbours.push(currentGrid[i][indexLeft]);
-  neighbours.push(currentGrid[i][indexRight]);
+  neighbours.push(currentGrid[currentField.y][indexLeft]);
+  neighbours.push(currentGrid[currentField.y][indexRight]);
   neighbours.push(currentGrid[indexBottom][indexLeft]);
-  neighbours.push(currentGrid[indexBottom][j]);
+  neighbours.push(currentGrid[indexBottom][currentField.x]);
   neighbours.push(currentGrid[indexBottom][indexRight]);
+  return neighbours;
 }
 
 function reset() {}
