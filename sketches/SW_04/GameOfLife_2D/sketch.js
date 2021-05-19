b59f58c58f52e9c63776127ce3f4e1945efa9672
Nicolas Vondru <niki.vondru@hotmail.ch>
2021-05-19 02:29:35 +0200

    add charts and play around with visualization

--- sketches/SW_04/GameOfLife_2D/sketch.js
+++ sketches/SW_04/GameOfLife_2D/sketch.js
@@ -1,3 +1,4 @@
+let chart;
 let currentGrid;
 let nextGrid;
 
@@ -5,17 +6,72 @@ let canvasWidth = 800;
 let canvasHeight = 800;
 
 let gridDimension = 100;
-let fieldWidth = canvasWidth / gridDimension;
-let fieldHeight = canvasHeight / gridDimension;
+let fieldWidth;
+let fieldHeight;
 
-let fps = 5;
+let aliveCells;
+
+let inputDimensions = document.getElementById("inputDimensions");
+let buttonStart = document.getElementById("buttonStart");
+let aliveMeter = document.getElementById("aliveMeter");
+
+let fps = 100;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+  createChart();
   frameRate(fps);
   // noStroke();
+
+  buttonStart.addEventListener("click", () => {
+    gridDimension = inputDimensions.value;
+    aliveMeter.max = gridDimension * gridDimension;
+    initializeSeedGrid();
+    drawCurrentGrid();
+  });
+
   // initialize seed
+  initializeSeedGrid();
+  drawCurrentGrid();
+}
+
+function createChart() {
+  var options = {
+    chart: {
+      type: "line",
+      animations: {
+        enabled: false,
+      },
+    },
+    series: [
+      {
+        name: "sales",
+        data: [],
+      },
+    ],
+    xaxis: {
+      labels: {
+        show: false,
+      },
+    },
+    stroke: {
+      width: 1,
+    },
+  };
+  chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+}
+
+function draw() {
+  background(220);
+  tick();
+  drawCurrentGrid();
+  updateUI();
+}
+
+function initializeSeedGrid() {
   currentGrid = [];
+  aliveCells = 0;
   for (let i = 0; i < gridDimension; i++) {
     let currentRow = [];
     for (let j = 0; j < gridDimension; j++) {
@@ -24,36 +80,41 @@ function setup() {
         y: i,
         isAlive: false,
       };
-      if (
-        i >= Math.floor(gridDimension / 2) - Math.floor(gridDimension / 5) &&
-        i <= Math.floor(gridDimension / 2) + Math.floor(gridDimension / 5) &&
-        j >= Math.floor(gridDimension / 2) - Math.floor(gridDimension / 5) &&
-        j <= Math.floor(gridDimension / 2) + Math.floor(gridDimension / 5)
-      ) {
-        currentRow[j].isAlive = true;
+      if (modeSelect.selectedOptions[0].value == "centered") {
+        if (
+          i >= Math.floor(gridDimension / 2) - Math.floor(gridDimension / 5) &&
+          i <= Math.floor(gridDimension / 2) + Math.floor(gridDimension / 5) &&
+          j >= Math.floor(gridDimension / 2) - Math.floor(gridDimension / 5) &&
+          j <= Math.floor(gridDimension / 2) + Math.floor(gridDimension / 5)
+        ) {
+          currentRow[j].isAlive = true;
+        }
+      } else if (modeSelect.selectedOptions[0].value == "random") {
+        if (Math.round(Math.random()) === 1) {
+          currentRow[j].isAlive = true;
+        }
+      } else {
       }
     }
     currentGrid.push(currentRow);
   }
-  drawCurrentGrid();
-}
-
-function draw() {
-  background(220);
-  tick();
-  drawCurrentGrid();
 }
 
 function drawCurrentGrid() {
+  fieldWidth = canvasWidth / gridDimension;
+  fieldHeight = canvasHeight / gridDimension;
   currentGrid.forEach((row) => {
     row.forEach((field) => {
       if (field.isAlive) {
-        fill(0);
+        fill(0, 0, 0, 255);
         stroke(0);
       } else {
-        fill(255);
+        fill(255, 255, 255, 50);
         stroke(180);
       }
+
+      let rndPos = random(1, 10);
+      let rndSize = random(1, 100);
       rect(
         field.x * fieldWidth,
         field.y * fieldHeight,
@@ -66,6 +127,7 @@ function drawCurrentGrid() {
 
 function tick() {
   nextGrid = [];
+  aliveCells = 0;
   for (let y = 0; y < gridDimension; y++) {
     nextGrid[y] = [];
   }
@@ -99,12 +161,16 @@ function tick() {
         currentField_copy.isAlive = true;
       }
 
+      if (currentField_copy.isAlive) {
+        aliveCells += 1;
+      }
       nextGrid[currentField.y][currentField.x] = currentField_copy;
     });
   });
   currentGrid = nextGrid;
 }
 
+// could be optimized for speed (positions must not be recalculated)
 function getNeighbours(currentField, rowCount, columnCount) {
   let neighbours = [];
   let indexTop;
@@ -151,4 +217,11 @@ function getNeighbours(currentField, rowCount, columnCount) {
   return neighbours;
 }
 
-function reset() {}
+function updateUI() {
+  aliveMeter.value = aliveCells;
+  chart.appendData([
+    {
+      data: [aliveCells],
+    },
+  ]);
+}
